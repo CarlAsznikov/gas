b4f0a6a3a9420562518e808d9731ba88b25f5653
CarlAsznikov <carl.asznikov@gmx.de>
2024-10-25 22:10:22 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,13 +6,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Verbindung zu MongoDB herstellen
-const mongoURI = 'mongodb+srv://carlasznikov:NZMOf3HdwMZM2Xjh@cluster0.pozoi.mongodb.net/gasMeterDB?retryWrites=true&w=majority&appName=Cluster0;
-    
+const mongoURI = 'mongodb+srv://carlasznikov:NZMOf3HdwMZM2Xjh@cluster0.pozoi.mongodb.net/gasMeterDB?retryWrites=true&w=majority&appName=Cluster0';
+
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB erfolgreich verbunden'))
     .catch(error => console.error('Fehler beim Verbinden mit MongoDB:', error));
 
-// Schema und Modell für Zählerstände definieren
 const meterReadingSchema = new mongoose.Schema({
     reading: Number,
     timestamp: { type: Date, default: Date.now }
@@ -25,11 +24,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // API-Endpunkt zum Speichern des Zählerstandes
 app.post('/save-meter-reading', async (req, res) => {
+    console.log('POST /save-meter-reading aufgerufen');
+    console.log('Anfrageinhalt:', req.body);
+
     try {
         const newReading = new MeterReading({
             reading: req.body.reading
         });
         await newReading.save();
+        console.log('Zählerstand erfolgreich gespeichert');
         res.send('Daten erfolgreich gespeichert');
     } catch (error) {
         console.error('Fehler beim Speichern der Daten:', error);
@@ -53,7 +56,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Server starten
 app.listen(PORT, () => {
     console.log(`Server läuft auf http://localhost:${PORT}`);
 });
